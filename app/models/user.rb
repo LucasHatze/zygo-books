6e8bf601f485a0151de5dc85f5d35c56d7f5660a
Lucas Matheus <lucas.silva.prog@gmail.com>
2021-05-04 00:07:00 -0300

    Add seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,5 +19,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :full_name, presence: true
-  validates_length_of :full_name, minimum: 5
+  validates_length_of :full_name, minimum: 4
 end
